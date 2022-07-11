8cd39fee78b8c7ab7e529022090e09a3f1a60c55
Evanilsonpg <99387388+Evanilsonpg@users.noreply.github.com>
2022-07-10 22:39:19 -0300

    adicionando icons

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,5 +1,9 @@
 import './style.css'
 import {FiMoreHorizontal} from 'react-icons/fi'
+import {IoMdHeartEmpty} from 'react-icons/io'
+import {BsChat} from 'react-icons/bs'
+import {FiSend} from 'react-icons/fi'
+import {BsBookmark} from 'react-icons/bs'
 
 export function Post () {
     return(
@@ -18,13 +22,42 @@ export function Post () {
                     <FiMoreHorizontal />
                 </div>
 
-            </header>
+            </header> 
 
             <div className='img-post'>
 
                 <img src='https://github.com/maykbrito.png' alt='img-post' />
 
             </div>
+
+            <div className='footer-post'>
+
+                <section className='engagements-post'>
+
+                    <div className='icons-1'>
+
+                        <div className='icon'>
+                            <IoMdHeartEmpty size={23} />
+                        </div>
+
+                        <div className='icon'>
+                            <BsChat size={20} />
+                        </div>
+
+                        <div className='icon'>
+                            <FiSend size={20} />
+                        </div>
+
+                    </div>
+
+                    <div className='icon'>
+                        <BsBookmark size={20} />
+                    </div>
+
+                </section>
+
+            </div>
+
         </>
     )
 }
